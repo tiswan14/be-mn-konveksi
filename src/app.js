30c995276ec3c3d7eee25885e43dec227b79a592
tiswan14 <tiswan30@gmail.com>
2026-01-06 15:47:15 +0700

    fix(cors): allow PATCH method for produk update & preflight OPTIONS

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ const corsOptions = {
         "https://mn-konveksi.vercel.app",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 };
 
