6260078b6417e831e3769b2c743f3c25fb97483e
tiswan14 <tiswan30@gmail.com>
2026-01-27 19:25:58 +0700

    fix: error cors in production

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ const corsOptions = {
         "http://localhost:5173",
         "http://localhost:3000",
         "https://mn-konveksi-fe.vercel.app",
-        "https://fe-mn-konveksi.vercel.app/"
+        "https://fe-mn-konveksi.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
